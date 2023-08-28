50177c8e28451f25fc6c72309e178cccbb58793c
Erdem Köşk(MEK) <erdemkosk@gmail.com>
2023-08-28 13:58:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const ip = require('ip');
 const app = require('./main.js');
 const config = require('./config');
 const logger = require('./api/plugin/logger');
+const val = process.env.VAL;
 
 app.listen(config.server.port, () => (config.swagger.enabled ? logger.info(`Express server is running at: \n - 
     Api: http://${ip.address()}:${config.server.port} \n - Swagger: http://${ip.address()}:${config.server.port}/api-docs`) :
